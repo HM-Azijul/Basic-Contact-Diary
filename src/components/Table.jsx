739978bebde4b0af1edfd2bfdc9f9c96b30d7f67
HM-Azijul <azijulhm@gmail.com>
2022-05-28 15:52:53 +0600

    update filter logic in Table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,19 +1,28 @@
 import { useState } from 'react';
 
 const Table = ({ contacts }) => {
-  const [filter, setFilter] = useState('All'); // it's a local state
+  const [filter, setFilter] = useState('All'); // it's a local state //useState('All') - default value 'All', None, Home, Office
   const handleChange = (e) => {
     setFilter(e.target.value);
   };
+
+  let filterContacts = [];
+  if (filter === 'All') {
+    filterContacts = contacts;
+  }
+  //else if (filter === 'None') { filterContacts = contacts.filter((contact) => contact.group === ''); }
+  else {
+    filterContacts = contacts.filter((contact) => contact.group === filter);
+  }
 
   return (
     <>
       <div>
         <br />
         Filters:
-        <select onChange={handleChange}>
+        <select value={filter} onChange={handleChange}>
           <option value="All">All</option>
-          <option value="None">None</option>
+          <option value="">None</option>
           <option value="Home">Home</option>
           <option value="Office">Office</option>
         </select>
@@ -28,7 +37,7 @@ const Table = ({ contacts }) => {
           </tr>
         </thead>
         <tbody>
-          {contacts.map((contact, index) => (
+          {filterContacts.map((contact, index) => (
             <tr key={index}>
               <td>{contact.name}</td>
               <td>{contact.email}</td>
