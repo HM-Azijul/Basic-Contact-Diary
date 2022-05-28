615051f16cc2527cb5f2b7817324b80695cb6ece
HM-Azijul <azijulhm@gmail.com>
2022-05-28 15:39:24 +0600

    add filter in Table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,21 +1,43 @@
+import { useState } from 'react';
+
 const Table = ({ contacts }) => {
+  const [filter, setFilter] = useState('All'); // it's a local state
+  const handleChange = (e) => {
+    setFilter(e.target.value);
+  };
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        {contacts.map((contact, index) => (
-          <tr key={index}>
-            <td>{contact.name}</td>
-            <td>{contact.email}</td>
+    <>
+      <div>
+        <br />
+        Filters:
+        <select onChange={handleChange}>
+          <option value="All">All</option>
+          <option value="None">None</option>
+          <option value="Home">Home</option>
+          <option value="Office">Office</option>
+        </select>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Group</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {contacts.map((contact, index) => (
+            <tr key={index}>
+              <td>{contact.name}</td>
+              <td>{contact.email}</td>
+              <td>{contact.group}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
