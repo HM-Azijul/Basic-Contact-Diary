fe67afeac0ea9fe76c8d6e56e698b2f02d70fef2
HM-Azijul <azijulhm@gmail.com>
2022-05-28 15:28:47 +0600

    add filter group

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,11 +3,12 @@ import { useState } from 'react';
 const CONTACT_FORM_INIT_STATE = {
   name: '',
   email: '',
+  group: '',
 };
 
 const ContactForm = ({ getContact }) => {
   const [values, setValues] = useState({ ...CONTACT_FORM_INIT_STATE });
-  const { name, email } = values;
+  const { name, email, group } = values;
 
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -43,6 +44,15 @@ const ContactForm = ({ getContact }) => {
           value={email}
           onChange={handleChange}
         />
+      </div>
+
+      <div>
+        <label htmlFor="group">Group:</label>
+        <select name="group" id="group" value={group} onChange={handleChange}>
+          <option value="">Select Group</option>
+          <option value="Home">Home</option>
+          <option value="Office">Office</option>
+        </select>
       </div>
 
       <br />
